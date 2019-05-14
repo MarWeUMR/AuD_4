9cbd6ab09feb80b008eb7d934ad35f413d2c3778
CodingAlwaysMessy <marcus.wallau@googlemail.com>
2019-05-14 22:13:59 +0200

    Char[] test ergänzt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Integer[] arr = new Integer[]{0,21,4,2,12,800,20,7,3,60,1};
+        Character[] crr = new Character[]{'a', 'c', 'e', 'a', 'g', 'b'};
 
-        PriorityQueueMaxHeap heap = new PriorityQueueMaxHeap(arr);
+        PriorityQueueMaxHeap heap = new PriorityQueueMaxHeap(crr);
 
         heap.heapSort();
 
