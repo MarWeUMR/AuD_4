901b78077bdb8bae0732039626924fa97044b1c9
CodingAlwaysMessy <marcus.wallau@googlemail.com>
2019-05-14 22:05:08 +0200

    Bugs fixed, build heap und heapify implementiert

--- src/com/company/PriorityQueueMaxHeap.java
+++ src/com/company/PriorityQueueMaxHeap.java
@@ -10,20 +10,22 @@ public class PriorityQueueMaxHeap implements PriorityQueue {
     private int heapsize = 0;
 
     public PriorityQueueMaxHeap(Comparable[] cRay) {
+        // Konstruktor arbeitet als Build Max Heap
 
-        a = new ArrayList<>();
-        a.add(cRay[0]);
+        a = new ArrayList<>(Arrays.asList(cRay));
+        heapsize = cRay.length - 1;
 
-        for (int j = 1; j < cRay.length; j++) {
-            addElement(cRay[j]);
+        for (int i = heapsize - 1 >> 1; i >= 0; i--) {
+            maxHeapify(i);
         }
+
         System.out.println(a);
     }
 
     private void increaseKey(int i, Comparable elem) {
 
         try {
-            if (i > 1 & elem.compareTo(a.get(i)) < 0) throw new Exception("alköjsdf");
+            if (i > 1 & elem.compareTo(a.get(i)) < 0) throw new Exception("Error...somewhere. Try 42.");
 
             a.set(i, elem);
 
@@ -33,6 +35,25 @@ public class PriorityQueueMaxHeap implements PriorityQueue {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private void maxHeapify(int i) {
+
+        int l = 2 * i;
+        int r = l + 1;
+
+        int max = (l <= heapsize && a.get(l).compareTo(a.get(i)) > 0)
+                ? l
+                : i;
+
+        if (r <= heapsize && a.get(r).compareTo(a.get(max)) > 0) {
+            max = r;
+        }
+
+        if (max != i) {
+            Collections.swap(a, i, max);
+            maxHeapify(max);
         }
     }
 
@@ -51,6 +72,29 @@ public class PriorityQueueMaxHeap implements PriorityQueue {
 
     @Override
     public void deleteFirst() {
+
+        try {
+            if (heapsize < 1) {
+                throw new Exception("Error...somewhere. Try 42.");
+            }
+
+            Comparable max = a.get(0);
+            a.set(1, a.get(heapsize));
+
+            heapsize--;
+
+            maxHeapify(1);
+
+            getFirst();
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void buildMaxHeap() {
+
 
     }
 }
